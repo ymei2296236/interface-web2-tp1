9acb79ccdc89b69e2e438b3e6fa15865f660db41
ymei2296236 <e2296236@cmaisonneuve.qc.ca>
2023-10-19 20:10:08 -0400

    Déclare les propriétés comme privées et ajout le test

--- assets/scripts/Librarie.js
+++ assets/scripts/Librarie.js
@@ -2,67 +2,77 @@ import { livres } from "./livres.js";
 import Livre from "./Livre.js";
 
 export default class Librarie {
+	#_el;
+	#_elsFiltre;
+	#_elsFiltreCategorie;
+	#_elControlLivres;
+
 	constructor(el) {
-		this._el = el;
-		this._elsFiltre = this._el.querySelectorAll("[data-js-filtre]");
-		this._elsFiltreCategorie = this._el.querySelectorAll(
-			"[data-js-filtre-categorie]"
-		);
-		this._elControlLivres = document.querySelector(
-			"[data-js-control-livres]"
-		);
-		this.init();
+		this.#_el = el;
+		this.#_elsFiltre = this.#_el.querySelectorAll("[data-js-filtre]");
+		this.#_elsFiltreCategorie = this.#_el.querySelectorAll("[data-js-filtre-categorie]");
+		this.#_elControlLivres = document.querySelector("[data-js-control-livres]");
+
+		this.#init();
 	}
 
-	init() {
+	/**
+	 * Section privée
+	 */
+	#init() {
+		// Injecte les 12 premiers livres au chargement de la page
 		for (let i = 0, l = 12; i < l; i++) {
 			new Livre(livres[i], i);
 		}
-		this.controleFiltres();
+		this.#afficheLibarieParFiltre();
 	}
 
-	controleFiltres() {
-		for (let i = 0, l = this._elsFiltre.length; i < l; i++) {
-			this._elsFiltre[i].addEventListener(
-				"click",
-				function () {
-					this._elControlLivres.innerHTML = "";
+	/**
+	 * Section privée
+	 * Affiche les livres correspondants au flitre cliqué
+	 */
+	#afficheLibarieParFiltre() {
 
-					let filtreActive = document.querySelector(".liActive");
+		for (let i = 0, l = this.#_elsFiltre.length; i < l; i++) {
+			// Ajoute un gestionnaire d'événement au chaque filtre
+			this.#_elsFiltre[i].addEventListener("click",function () {
+				
+				// Gére le style de filtre 
+				let filtreActive = document.querySelector(".filtreActive");
+				
+				if (filtreActive !== null) {
+					filtreActive.classList.remove("filtreActive");
+				}
+				this.#_elsFiltre[i].classList.add("filtreActive");
+				// Réinitialise la zone d'affichage de livres
+				this.#_elControlLivres.innerHTML = "";
+				
+				/**
+				 * Effectue le filtrage en fonction de filtre
+				 */
+				let nomFiltre = this.#_elsFiltre[i].dataset.jsFiltre,
+					nomCategorie = this.#_elsFiltre[i].dataset.jsCategorie;
 
-					if (filtreActive !== null) {
-						filtreActive.classList.remove("liActive");
+				if (nomFiltre == "tous") {
+					for (let i = 0, l = livres.length; i < l; i++) {
+						new Livre(livres[i], i);
 					}
-					this._elsFiltre[i].classList.add("liActive");
+				}
 
-					// if (this._elsFiltre[i].classList.contains("liActive")) {
-					// 	this._elsFiltre[i].classList.remove("liActive");
-					// } else this._elsFiltre[i].classList.add("liActive");
-
-					let nomFiltre = this._elsFiltre[i].dataset.jsFiltre,
-						nomCategorie = this._elsFiltre[i].dataset.jsCategorie;
-
-					if (nomFiltre == "tous") {
-						for (let i = 0, l = livres.length; i < l; i++) {
+				if (nomFiltre == "nouveaute") {
+					for (let i = 0, l = livres.length; i < l; i++) {
+						if (livres[i][nomFiltre] === true)
 							new Livre(livres[i], i);
-						}
 					}
+				}
 
-					if (nomFiltre == "nouveaute") {
-						for (let i = 0, l = livres.length; i < l; i++) {
-							if (livres[i][nomFiltre] == true)
-								new Livre(livres[i], i);
-						}
+				if (nomFiltre == "categorie" && nomCategorie) {
+					for (let i = 0, l = livres.length; i < l; i++) {
+						if (livres[i].categorie == nomCategorie)
+							new Livre(livres[i], i);
 					}
-
-					if (nomFiltre == "categorie" && nomCategorie) {
-						for (let i = 0, l = livres.length; i < l; i++) {
-							if (livres[i].categorie == nomCategorie)
-								new Livre(livres[i], i);
-						}
-					}
-				}.bind(this)
-			);
+				}
+			}.bind(this));
 		}
 	}
 }
--- assets/scripts/Panier.js
+++ assets/scripts/Panier.js
@@ -1,76 +1,75 @@
-import { livres } from "./livres.js";
-
 export default class Panier {
+	#_el;
+	#_btnPanier;
+	#_elPanierDetail;
+
 	constructor(el) {
-		this._el = el;
-		this._btnPanier = this._el.querySelector("[data-js-panier-bouton]");
-		this._elPanierDetail = this._el.querySelector("[data-js-detail]");
-		this._elBody = this._el.closest("body");
-		this.init();
+		this.#_el = el;
+		this.#_btnPanier = this.#_el.querySelector("[data-js-panier-bouton]");
+		this.#_elPanierDetail = this.#_el.querySelector("[data-js-detail]");
+
+		this.#init();
 	}
 
-	init() {
-		this._btnPanier.addEventListener(
-			"click",
-			function () {
-				this.afficheModal();
-			}.bind(this)
-		);
+	/**
+	 * Section privée
+	 */
+	#init() {
+		// Ajoute un gestionnaire d'événement à l'icône de panier
+		this.#_btnPanier.addEventListener("click", function () {
+			this.#controleModal();
+		}.bind(this));
 	}
 
-	afficheModal() {
-		if (this._elPanierDetail.classList.contains("panier__detail--ferme")) {
-			this._elPanierDetail.classList.replace(
-				"panier__detail--ferme",
-				"panier__detail--ouvert"
-			);
-			this.injecteDom();
-		} else if (
-			this._elPanierDetail.classList.contains("panier__detail--ouvert")
-		) {
-			this._elPanierDetail.classList.replace(
-				"panier__detail--ouvert",
-				"panier__detail--ferme"
-			);
-			this._elPanierDetail.innerHTML = "";
+	/**
+	 * Gère le modal de panier
+	 */
+	#controleModal() {
+		if (this.#_elPanierDetail.classList.contains("panier__detail--ferme")) {
+			this.#_elPanierDetail.classList.replace("panier__detail--ferme","panier__detail--ouvert");
+			this.#injecteDom();
+		} 
+		else if (this.#_elPanierDetail.classList.contains("panier__detail--ouvert")) {
+			this.#_elPanierDetail.classList.replace("panier__detail--ouvert", "panier__detail--ferme");
+			this.#_elPanierDetail.innerHTML = "";
 		}
 	}
 
-	injecteDom() {
-		let somme = 0;
+	/**
+	 * Crée et injecte le dom au modal de panier
+	 */
+	#injecteDom() {
+		let prixTotal = 0;
+
 		if (!localStorage.panier) {
-			this._elPanierDetail.innerHTML = `
-				<p>Il n'y a aucun livre dans votre panier.</p>
-				`;
-		} else {
-			this._aPanier = JSON.parse(localStorage.panier);
-			let nbLivre = Object.keys(this._aPanier).length;
-			this._elPanierDetail.innerHTML = `
-											<table>
-												<thead>
-													<tr>
-													<th>Livre${nbLivre > 1 ? "s" : ""}</th>
-													<th>Prix</th>
-													</tr>
-												</thead>
-												<tbody>
-												</tbody>
-											</table>
+			this.#_elPanierDetail.innerHTML = `
+												<p>Il n'y a aucun livre dans votre panier.</p>
 											`;
-			this._tbody = this._elPanierDetail.querySelector("tbody");
-
-			for (const key in this._aPanier) {
-				this._tbody.insertAdjacentHTML(
-					"beforeend",
-					`<td>${key}</td><td>${this._aPanier[key]} $</td>`
-				);
-
-				somme += parseInt(this._aPanier[key]);
+		} 
+		else {
+			let aPanier = JSON.parse(localStorage.panier),
+				nbLivre = Object.keys(aPanier).length;
+			// Crée le dom de panier
+			this.#_elPanierDetail.innerHTML = `
+												<table>
+													<thead>
+														<tr>
+															<th>Livre${nbLivre > 1 ? "s" : ""}</th>
+															<th>Prix</th>
+														</tr>
+													</thead>
+													<tbody>
+													</tbody>
+												</table>
+											`;
+			let elTBody = this.#_elPanierDetail.querySelector("tbody");
+			// Injecte la liste de livres ajoutés au panier
+			for (const cle in aPanier) {
+				elTBody.insertAdjacentHTML("beforeend", `<td>${cle}</td><td>${aPanier[cle]} $</td>`);
+				prixTotal += parseInt(aPanier[cle]);
 			}
-			this._tbody.insertAdjacentHTML(
-				"beforeend",
-				`<td>Total</td><td class="panier__somme">${somme} $</td>`
-			);
+			// Injecte le prix total au panier
+			elTBody.insertAdjacentHTML("beforeend",`<td>Total</td><td class="panier__prixTotal">${prixTotal} $</td>`);
 		}
 	}
 }
